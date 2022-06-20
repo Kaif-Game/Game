249aa195b823e29bbfd1a9c1ecd7785b1915efe8
Kiryl Pryhozhy <kirill19962003@gmail.com>
2022-06-20 20:09:49 +0300

    add jump

--- game/Assets/Scripts/PlayerMovement.cs
+++ game/Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Rigidbody2D rigidbody;
+    [SerializeField] private float jumpForce = 7f;  
+
     void Start()
     {
-        
+        rigidbody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown("space"))
+        {
+            rigidbody.velocity = new Vector2(rigidbody.velocity.x, jumpForce);
+        }
     }
 }
