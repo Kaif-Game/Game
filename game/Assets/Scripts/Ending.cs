078767800214d12cdda0e1c10603183c44fb8889
Victor <siva500000217@mail.ru>
2022-08-27 21:57:34 +0300

    add final

---
+++ game/Assets/Scripts/Ending.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Ending : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "Player")
+        {
+            SceneManager.LoadScene("Menu");
+        }
+    }
+}
