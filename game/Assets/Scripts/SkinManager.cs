1b26ba611ed3f02f2c5629f162fbc3d5d3d88ce4
Kiryl Pryhozhy <kirill19962003@gmail.com>
2022-08-31 20:24:17 +0300

    add music in main menu

---
+++ game/Assets/Scripts/SkinManager.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkinManager : MonoBehaviour
+{
+     private Sprite currentSprite;
+    void Start()
+    {
+        
+    }
+
+    public void ChangeCurrentSkin()
+    {
+
+    }
+    
+}
